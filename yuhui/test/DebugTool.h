57aa01660a10dc6eb8636fabcce72d60c43ec51f
yinglang <y19941010@126.com>
2016-01-25 11:23:00 +0800

    delete hui

--- yuhui/test/DebugTool.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUGTOOL_H
-#define DEBUGTOOL_H
-
-#include "DJI_LIB/DJI_Pro_App.h"
-void printFrame(unsigned char* buf, int len);
-void print(activate_data_t user_act_data);
-
-#endif // DEBUGTOOL_H
